814d3d9b329b0591cac4b0c52b4810c9058d014b
pseidel <paul.seidel@tu-dresden.de>
2024-08-27 15:11:00 +0200

    update types

--- controller_software/config/types.py
+++ controller_software/config/types.py
@@ -39,7 +39,7 @@ class ControllerComponents(Enum):
 
     # TODO: Which components are needed?
 
-    STORAGE = "storage"
+    STORAGE = "storage_controller"
     HYGIENICCONTROLLER = "hygienic_controller"
 
 class DataQueryTypes(Enum):
